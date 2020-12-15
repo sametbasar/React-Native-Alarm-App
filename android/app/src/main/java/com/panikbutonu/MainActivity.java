50c2801f589038fae7f643ebd81f958106d848a0
Samet Başar <sametbbasar@gmail.com>
2020-12-15 15:53:39 +0300

    changed app name

--- android/app/src/main/java/com/panikbutonu/MainActivity.java
+++ android/app/src/main/java/com/panikbutonu/MainActivity.java
@@ -1,4 +1,4 @@
-package com.pronetmobileapp;
+package com.panikbutonu;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "PronetMobileApp";
+    return "PanikButonu";
   }
 }
